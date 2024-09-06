e6e0de7246e0ebdec8aea7cacbcba01e947d276d
corlys <duscae047@gmail.com>
2024-09-06 20:10:26 +0700

    feat: invalid email handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+import { Toaster } from "@/components/ui/toaster";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -28,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster />
         {children}
       </body>
     </html>
--- components/EditableCell.tsx
+++ components/EditableCell.tsx
@@ -2,6 +2,7 @@ import { DataType } from "@/app/ambisius/columns";
 import { CellContext } from "@tanstack/react-table";
 import { useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
+import { useToast } from "@/hooks/use-toast";
 
 interface EditableCellProps extends CellContext<DataType, unknown> {
   newRow?: boolean;
@@ -11,6 +12,8 @@ export default function EditableCell(props: EditableCellProps) {
   const initialValue = props.getValue();
   const [value, setValue] = useState(initialValue);
   const [isEdited, setIsEdited] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const { toast } = useToast();
 
   useEffect(() => {
     setValue(initialValue);
@@ -18,19 +21,40 @@ export default function EditableCell(props: EditableCellProps) {
 
   useEffect(() => {
     setIsEdited(false);
+    setIsError(false);
   }, [props.table.options.meta?.resetCells]);
 
   const onBlur = () => {
     const meta = props.table.options.meta;
+    if (props.column.id === "email") {
+      const validEmail = isValidEmail(value as string);
+      console.log("check email ", validEmail);
+      setIsError(!validEmail);
+      if (!validEmail) {
+        toast({
+          variant: "destructive",
+          title: "Invalid Email",
+          description: "Please fix email format before proceeding",
+        });
+      }
+    }
     meta?.updateData(props.row.index, props.column.id, value);
     meta?.handleEditRow(props.row.index);
     setIsEdited(true);
   };
 
+  function isValidEmail(email: string): boolean {
+    const pattern: RegExp = /^[\w\.-]+@[\w\.-]+\.\w+$/;
+    return pattern.test(email);
+  }
+
   return (
     <>
       <Input
-        className={`${(isEdited || props?.newRow) && "bg-green-200"}`}
+        className={`
+          ${(isEdited || props?.newRow) && !isError && "bg-green-200"}
+          ${isError && "bg-red-200"}
+        `}
         onBlur={onBlur}
         value={value as string}
         onChange={(e) => setValue(e.target.value)}
